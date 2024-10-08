ffea06837f92214d1dd1d2240f892150f6e33a8a
floo69 <codeit69420@gmail.com>
2024-10-08 23:46:23 +0530

    s commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import ListGroup from "./Components/ListGroup";
+import Alert from "./Components/Alert";
+// import ListGroup from "./Components/ListGroup";
 
-function App(){
-  return <div><ListGroup/></div>
+function App() {
+  // let items = ["Mumabi", "Delhi", "Pune", "Bangalore", "Gurgaon"];
+
+  // const handleSelectItem = (item: string) => {
+  //   console.log(item);
+  // };
+  return (
+    <div>
+      <Alert>Hello <span>World</span></Alert>
+      {/* <ListGroup
+        items={items}
+        heading={"Cities"}
+        onSelectItem={handleSelectItem}
+      /> */}
+    </div>
+  );
 }
 
 export default App;
